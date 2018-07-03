a2f0266950ddaeedb38e070c2c65bf609c6c6ffe
pijush karmakar <pijushkarmakar45@gmail.com>
2018-07-03 15:21:26 +0600

    bloodbank

--- BloodBankProject/about.php
+++ BloodBankProject/about.php
@@ -14,23 +14,18 @@
 				 		<div class="container">
 				 			<div class="row">
 				 			<div class="col-md-8 ab-text">
-				 				<img src="images/ab.jpg" alt=" " />
-				 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu erat lacus, vel congue mauris. Fusce velit justo, faucibus eu sagittis ac, gravida quis tortor. Suspendisse non urna mi, quis tincidunt eros. Nullam tellus turpis, fringilla sit amet congue ut, luctus a nulla. Donec sit amet sapien neque, id ullamcorper diam. Nulla facilisi. Pellentesque pellentesque arcu a elit congue lacinia.</p>
-								<ul class="ab">
-									<li><a href="single.html">Lorem ipsum dolor sit amet,consectetur adipiscing elit</a></li>
-									<li><a href="single.html">Pellentesque eu erat lacus,consectetur adipiscing elit</a></li>
-									<li><a href="single.html">Lorem ipsum dolor sit amet,consectetur adipiscing elit</a></li>
-									<li><a href="single.html">Lorem ipsum dolor sit amet,consectetur adipiscing elit</a></li>
-									<li><a href="single.html">Pellentesque eu erat lacus,consectetur adipiscing elit</a></li>
-								</ul>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu erat lacus, vel congue mauris. Fusce velit justo, faucibus eu sagittis ac, gravida quis tortor. Suspendisse non urna mi, quis tincidunt eros. Nullam tellus turpis, fringilla sit amet congue ut, luctus a nulla. Donec sit amet sapien neque, id ullamcorper diam. Nulla facilisi. Pellentesque pellentesque arcu a elit congue lacinia.</p>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu erat lacus, vel congue mauris. Fusce velit justo, faucibus eu sagittis ac, gravida quis tortor. Suspendisse non urna mi, quis tincidunt eros. Nullam tellus turpis, fringilla sit amet congue ut, luctus a nulla. Donec sit amet sapien neque, id ullamcorper diam. Nulla facilisi. Pellentesque pellentesque arcu a elit congue lacinia.</p>
+
+				 			<p><img src="Images/123.jpg" height="250px" /> Life Saver Blood Bank' is the first product resulted out of the community welfare initiative called 'People Project' from uSiS Technologies. Universally, 'Blood' is recognized as the most precious element that sustains life. It saves innumerable lives across the world in a variety of conditions. Once in every 2- seconds, someone, somewhere is desperately in need of blood. More than 29 million units of blood components are transfused every year. The need for blood is great - on any given day, approximately 39,000 units of Red Blood Cells are needed. Each year, we could meet only up to 1% (approx) of our nation’s demand for blood transfusion.Despite the increase in the number of donors, blood remains in short supply during emergencies, mainly attributed to the lack of information and accessibility.</p>
+							<p class="top"> We positively believe this tool can overcome most of these challenges by effectively connecting the blood donors with the blood recipients.
+					        We remind every visitor that we have the empowerment to save lives and let’s do that – right now, right here. If you are eligible for blood donation, please register yourself as a blood donor now!</p>
+							<p class="top">We also take this opportunity to thank our whole team for all your ideas, commitment and hard-ship in making this dream a reality. We would also like to thank our friends and well-wishers for all your support and encouragement throughout this project. It is now reasonably safe to say that together we have made this society a slightly better and safer place to live.</p>
+
 				 			</div>
 
 				 			<div class="col-md-4 ab-pic">
 				 				<div>
-				 					<img src="images/ab.jpg" alt=" " />	
-                                    <img src="images/ab.jpg" alt=" " />
+				 					<img src="images/ad1.jpg" alt=" " width="250px" height="300px" class="tableborder" /> <br> <br>	
+                                    <img src="images/ad3.jpg" alt=" " width="250px" height="500px" class="tableborder" />
 				 				</div>
 				 				 
 				 			</div>
--- BloodBankProject/index.php
+++ BloodBankProject/index.php
@@ -7,15 +7,53 @@
     <div class="custom_grid" id="tender">
         <div class="container">
             <div class="main1">
-                <div class="c_left">
+                <div class="c_left index">
                     <div class="left">
-                        <h2>Welcome</h2>
-                        <h4>Lorem Ipsum is simply dummy text of the printing,</h4>
-                        <p>Nam ornare vulputate risus,id volutpat elit porttitor non.In consequat nisi vel lectus dapibus sodales.Nam ornare vulputate risus, id volutpat elit porttitor non. In consequat nisi vel lectus dapibus sodales.Nam ornare vulputate risus, id volutpat elit porttitor non. In consequat nisi vel lectus dapibus sodales.Nam ornare vulputate risus, id volutpat elit porttitor non. In consequat nisi vel lectus dapibus sodales. Pellentesque habitant morbi tristique senectus Nam ornare vulputate risus, id volutpat elit porttitor non. In consequat nisi vel lectus dapibus sodales. PellentesqueNam ornare vulputate risus, id volutpat elit porttitor non. In consequat nisi vel lectus dapibus sodales. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas Pellentesque habitant morbi tristique senectus et netus et malesuada fames.</p>
+                        <img src="images/welcome.png" alt="">
+                        <p>Blood is universally recognized as the most precious element that sustains life. It saves innumerable lives across the world in a variety of conditions. The need for blood is great - on any given day, approximately 39,000 units of Red Blood Cells are needed. More than 29 million units of blood components are transfused every year. Donate Blood Despite the increase in the number of donors, blood remains in short supply during emergencies, mainly attributed to the lack of information and accessibility. We positively believe this tool can overcome most of these challenges by effectively connecting the blood donors with the blood recipients.</p> <br><br>
+
+                        <h4 class="bank">Blood bank</h4>
+                        <p>We welcome you to in our WebSite. If you are a donor , We appreciate you <a href="login.php">signing up</a> online as a Donor. If you need blood we are happy to serve you. This blood donor list is hosted by <a href="www.impijush.com">www.impijush.com</a> on behalf of "Life Saver Blood Bank"
+                        as a public service without any profit motive.This is a free service. While the Organisers have taken all steps to obtain accurate and up-to-date information of potential blood donors,the Organisers and ICM Computers do not guarantee accuracy of the information contained herein or the suitability of the persons listed as any liability for direct or consequential damage to any person using this blood donor list including 
+                        loss of life or damage due to infection of any nature arising out of blood transfusion from persons whose names have been listed in this website.
+                        We request donors to update contact details regularly.</p>
                     </div>
                 </div>
 
-                <div class="clear"></div>
+                <div class="c_right"> <!-- c_right -->
+                    <div class="sidebar"> <!-- sidebar -->
+                        <h2>Latest News</h2>
+                          <div class="blog_posts">
+                            <marquee direction="up" scrolldelay="150">
+                               <table >
+                                 <tr><td>
+                                    <div class="blog_desc">
+                                       <div class="blog_heading">
+                                            <span>HDFC Bank sets Guinness record in blood collection</span>
+                                            <p>The blood donation camp organised by HDFC Bank on December 6, 2013 involving 61,902 participants has found a place in Guinness Book of Records as the largest such drive on a single day</p>
+                                       </div> 
+                                       <br/>
+                                       <div class="blog_heading">
+                                            <span>World Blood Donor Day: Poor health reduces women blood donors in India</span>
+                                            <p>While the overall number of blood donors in India has grown over the years, women constitute only a tiny 10 percent share owing to health problems like pernicious anaemia and low haemoglobin levels or being underweight</p>
+                                        </div>  
+                                        <br>
+                                        <div class="blog_heading">
+                                            <span>Guinness record in blood collection</span>
+                                            <p>The blood donation camp organised by HDFC Bank on December 6, 2013 involving 61,902 participants has found a place in Guinness Book of Records as the largest such drive on a single day</p>
+                                       </div>
+
+                                    </div>
+                                </td></tr>
+                            </table>
+                         </marquee>  
+                         <div class="clear"></div>  
+                      </div>           
+                    </div> <!-- sidebar -->
+                 <div class="clear"></div>
+                </div> <!-- c_right -->
+
+                
             </div>
         </div>
     </div>
